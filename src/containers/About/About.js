7614f67d7d1a5b4d4ea97e15e8e393c87fdb2f6e
Isaac Hinman <isaac@isaachinman.com>
2018-10-11 12:50:34 +0100

    Add About page

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { translate } from 'utils/i18n'
+import { Col, Grid, Row } from 'react-flexbox-grid'
 import { Head } from 'components'
+import { translate } from 'utils/i18n'
 
 import styles from './About.styles'
 
@@ -19,7 +20,51 @@ export default class About extends React.Component {
         />
         <style jsx>{styles}</style>
         <div className='container'>
-          <h1>{t('title')}</h1>
+          <Grid>
+            <div className='hero'>
+              <Row>
+                <Col xs={10} xsOffset={1} md={8} mdOffset={2} lg={6} lgOffset={3}>
+                  <h1>{t('title')}</h1>
+                  <h2>{t('headline')}</h2>
+                  <p>{t('disconnect')}</p>
+                </Col>
+              </Row>
+              <Row>
+                <Col xs={10} xsOffset={1} md={4} mdOffset={2} lg={3} lgOffset={3}>
+                  <h3>{t('photographyToday.title')}</h3>
+                  <p>{t('photographyToday.line1')}</p>
+                  <p>{t('photographyToday.line2')}</p>
+                  <p>{t('photographyToday.line3')}</p>
+                  <p>{t('photographyToday.line4')}</p>
+                </Col>
+                <Col xs={10} xsOffset={1} md={4} lg={3} mdOffset={0}>
+                  <h3>{t('photographyTraditional.title')}</h3>
+                  <p>{t('photographyTraditional.line1')}</p>
+                  <p>{t('photographyTraditional.line2')}</p>
+                  <p>{t('photographyTraditional.line3')}</p>
+                  <p>{t('photographyTraditional.line4')}</p>
+                </Col>
+              </Row>
+            </div>
+            <Row>
+              <Col xs={10} xsOffset={1} md={8} mdOffset={2} lg={6} lgOffset={3}>
+                <h3>{t('reimagining.title')}</h3>
+                <p>{t('reimagining.description')}</p>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={10} xsOffset={1} md={8} mdOffset={2} lg={6} lgOffset={3}>
+                <h2>{t('joinUs.title')}</h2>
+                <p>{t('joinUs.description')}</p>
+              </Col>
+            </Row>
+            <Row>
+              <Col xs={10} xsOffset={1} md={8} mdOffset={2} lg={6} lgOffset={3}>
+                <h2>{t('press.title')}</h2>
+                <p>{t('press.description')}</p>
+              </Col>
+            </Row>
+          </Grid>
         </div>
       </React.Fragment>
     )
--- src/containers/About/About.styles.js
+++ src/containers/About/About.styles.js
@@ -2,7 +2,25 @@ import css from 'styled-jsx/css'
 
 export default css`
   .container {
-    display: flex;
-    background-color: green;
+    text-align: center;
+    padding: 50px 0 100px 0;
+    background: rgb(252,252,252);
+    background: radial-gradient(circle, rgba(252,252,252,1) 0%, rgba(244,244,244,1) 27%, rgba(250,250,250,1) 54%, rgba(217,217,217,1) 100%);
   }
+  .hero {
+    padding: calc(50vh - 300px) 0;
+  }
+  h1 {
+    font-family: 'Vollkorn', serif;
+    text-transform: uppercase;
+    font-size: 30px;
+    font-weight: 400;
+  }
+  h3 {
+    color: #333;
+    font-size: 20px;
+    font-weight: 400;
+    font-style: italic;
+  }
+
 `
